f087285e3868fd83d216f24eb576f2555dc9538e
joseilagosb <joseilagosb@gmail.com>
2024-08-27 17:24:36 -0400

    moved availability intervals logic to lib, created tests

--- app/services/shift_scheduler_service.rb
+++ app/services/shift_scheduler_service.rb
@@ -1,4 +1,6 @@
 class ShiftSchedulerService
+  attr_reader :service_id, :availability, :week
+
   def initialize(service_id, availability, week)
     @service_id = service_id
     @availability = availability
@@ -6,34 +8,18 @@ class ShiftSchedulerService
   end
 
   def call
-    availability_intervals = to_intervals
-    puts availability_intervals
+    availability_intervals = IntervalManager.build_intervals(@availability)
+    calculate_initial_shifts(availability_intervals)
   end
 
   private
 
-  attr_reader :service_id, :availability, :week
+  def calculate_initial_shifts(intervals)
+    puts intervals
+    puts 'calculate_initial_shifts'
+  end
 
-  def to_intervals
-    @availability['serviceDays'].each_with_object({}) do |day, result|
-      intervals_by_user = Hash.new { |hash, key| hash[key] = [] }
-      prev_available = {}
-      day['serviceHours'].each do |hour|
-        hour['available'].each do |user_id, available|
-          next unless available
-
-          current_interval = [hour['hour'], hour['hour']]
-          unless intervals_by_user[user_id].empty? || (prev_available.key?(user_id) && prev_available[user_id] == false)
-            current_interval = intervals_by_user[user_id].pop
-          end
-
-          current_interval[1] = hour['hour'] if current_interval[1] == hour['hour'] - 1
-
-          intervals_by_user[user_id] << current_interval
-        end
-        prev_available = hour['available']
-      end
-      result[day['day']] = intervals_by_user
-    end
+  def hours_in_week
+    @availability['serviceDays'].sum { |day| day['serviceHours'].size }
   end
 end
---
+++ spec/lib/interval_manager_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+describe IntervalManager do
+  let(:availabilities) do
+    JSON.parse(File.read('spec/fixtures/availability.json'))
+  end
+  let(:expected_intervals) { JSON.parse(File.read('spec/fixtures/interval.json')) }
+
+  context 'with valid parameters' do
+    # Represents a monday to tuesday availability, with the availability of two users of ids 101 and 102
+    # Both users are available everyday albeit in different time slots
+    context 'base availability' do
+      let(:intervals) { IntervalManager.build_intervals(availabilities['base']) }
+
+      it 'returns the correct number of days' do
+        puts intervals
+        expect(intervals.length).to eq(2)
+      end
+
+      it 'returns the correct number of users for each day' do
+        intervals.each_value do |day|
+          expect(day.length).to eq(2)
+        end
+      end
+
+      it 'returns the correct number of intervals for each user' do
+        intervals.each_value do |day|
+          day.each_value do |interval|
+            expect(interval.length).to eq(1)
+          end
+        end
+      end
+
+      it 'returns the correct intervals for each user in each day' do
+        expected_intervals_monday = { '101' => [[8, 9]], '102' => [[9, 10]] }
+        intervals['1'].each do |user, interval|
+          expect(interval).to eq(expected_intervals_monday[user])
+        end
+        expected_intervals_tuesday = { '101' => [[8, 8]], '102' => [[8, 10]] }
+        intervals['2'].each do |user, interval|
+          expect(interval).to eq(expected_intervals_tuesday[user])
+        end
+      end
+
+      it 'returns the expected intervals hash' do
+        expect(intervals).to eq(expected_intervals['base'])
+      end
+    end
+
+    context 'with two intervals in a day' do
+      let(:intervals_with_two_intervals_in_a_day) do
+        IntervalManager.build_intervals(availabilities['with_two_intervals_in_a_day'])
+      end
+
+      it 'returns two intervals in a single day' do
+        intervals_with_two_intervals_in_a_day['1'].each_value do |interval|
+          expect(interval.length).to eq(2)
+        end
+      end
+
+      it 'returns the expected intervals hash' do
+        expect(intervals_with_two_intervals_in_a_day).to eq(expected_intervals['with_two_intervals_in_a_day'])
+      end
+    end
+
+    context 'with unavailable user in a day' do
+      let(:intervals_with_unavailable_user) do
+        IntervalManager.build_intervals(availabilities['with_unavailable_user'])
+      end
+
+      before :each do
+        @monday_intervals = intervals_with_unavailable_user['1']
+        @tuesday_intervals = intervals_with_unavailable_user['2']
+      end
+
+      it 'returns the correct number of intervals for each user' do
+        expect(@monday_intervals.length).to eq(1)
+        expect(@tuesday_intervals.length).to eq(2)
+      end
+
+      it 'does not contain user with unavailable hours' do
+        expect(@monday_intervals).not_to include('101')
+        expect(@tuesday_intervals).to include('101')
+      end
+
+      it 'returns the expected intervals hash' do
+        expect(intervals_with_unavailable_user).to eq(expected_intervals['with_unavailable_user'])
+      end
+    end
+
+    context 'empty availability' do
+      let(:intervals_with_empty_availability) do
+        IntervalManager.build_intervals(availabilities['empty'])
+      end
+
+      it 'returns an empty hash for each day' do
+        intervals_with_empty_availability.each_value do |day|
+          expect(day).to eq({})
+        end
+      end
+
+      it 'returns the expected intervals hash' do
+        expect(intervals_with_empty_availability).to eq(expected_intervals['empty'])
+      end
+    end
+  end
+
+  context 'with invalid parameters' do
+    it 'raises an ArgumentError' do
+      expect { IntervalManager.build_intervals(nil) }.to raise_error(ArgumentError)
+    end
+  end
+end
